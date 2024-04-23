16940b4235ff96f11038074ae5a4411600af40c4
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-04-23 16:25:18 +0530

    Way too hopeful attempt.

--- token.ts
+++ token.ts
@@ -4,6 +4,7 @@ import {P, match} from "ts-pattern";
 export const tokenizer = (input: string): Token[] => {
     const tokens: Token[] = [];
     let ptr = 0;
+    let currentString = '';
     for (let char of input) {
         ptr++;
         match(char)
@@ -13,7 +14,40 @@ export const tokenizer = (input: string): Token[] => {
             .with("]", () => tokens.push({type: "BracketClose", value: char}))
             .with(":", () => tokens.push({type: "Colon", value: char}))
             .with(",", () => tokens.push({type: "Comma", value: char}))
-
+            .with('"', () => {
+                let value = "";
+                char = input[ptr++];
+                while (char !== '"') {
+                    value += char;
+                    char = input[ptr++];
+                }
+                tokens.push({type: "String", value})
+                ptr++;
+            })
+            .with(P.string.regex(/[\d\w]/), () => {
+                let value = "";
+                while(/[\d\w]/.test(char)) {
+                    value += char;
+                    char = input[++ptr];
+                }
+                match(value)
+                .with(P.number, () => tokens.push({type: "Number", value}))
+                .with(P.nullish, () => tokens.push({type: "Null", value}))
+                .with(P.boolean, () => {
+                    if (value === "true") {
+                        tokens.push({type: "True", value});
+                    } else {
+                        tokens.push({type: "False", value});
+                    }
+                })
+                .otherwise(() => {
+                    throw new Error("Unrecognized Value: " + value);
+                })
+            })
+            .with(P.string.regex(/\s/), () => {})
+            .otherwise(() => {
+                throw new Error("Unexpected Character");
+            })
     }
     return tokens;
 }
