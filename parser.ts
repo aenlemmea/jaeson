9a93f6999b50de3170ebba8b0e5faa7593d5f880
aenlemmea <118059484+aenlemmea@users.noreply.github.com>
2024-04-23 18:58:13 +0530

    Making the compiler gods happy. Pattern matching failing hard to impress here.

--- parser.ts
+++ parser.ts
@@ -14,39 +14,59 @@ export const parser = (tokens: Token[]): ASTNode => {
 
     function parseValue() : ASTNode {
         const token = tokens[counter];
+        let result: ASTNode = { type: "Null"}; ;
         match(token.type)
-        .with("String", () => {
-            return {type: "String", value: token.value};
-        })
-        .with("Number", () => {
-            return {type: "Number", value: Number(token.value)};
-        })
-        .with("True", () => {
-                return {type: "Boolean", value: true};
-        })
-        .with("False", () => {
-            return {type: "Boolean", value: false};
-        })
-        .with("Null", () => {
-            return {type:"Null"};
-        })
-        .with("BraceOpen", () => {
-            return parseObject();
-        })
-        .with("BracketOpen", () => {
-            return parseArray();
-        })
-        .otherwise(() => {
-            throw new Error(`Unexpected token type: ${token.type}`);
-        })
+            .with("String", () => {
+                result = {type: "String", value: token.value};
+            })
+            .with("Number", () => {
+                result = {type: "Number", value: Number(token.value)};
+            })
+            .with("True", () => {
+                result = {type: "Boolean", value: true};
+            })
+            .with("False", () => {
+                result = {type: "Boolean", value: false};
+            })
+            .with("Null", () => {
+                result = {type:"Null"};
+            })
+            .with("BraceOpen", () => {
+                result = parseObject();
+            })
+            .with("BracketOpen", () => {
+                result = parseArray();
+            })
+            .otherwise(() => {
+                throw new Error(`Unexpected token type: ${token.type}`);
+            });
+
+        return result;
     }
-}
-
-function parseObject(): any {
-    throw new Error("Function not implemented.");
-}
 
 
-function parseArray() {
-    throw new Error("Function not implemented.");
+    function parseObject() : ASTNode {
+        const current : ASTNode = {type: "Object", value: {}};
+        let token = next();
+
+        while(token.type !== "BraceClose") {
+            if(token.type === "String") {
+                const key = token.value;
+                token = next();
+                if (token.type !== "Colon") throw new Error("Expexted: in key-value pair");
+                token = next();
+                const value = parseValue();
+                current.value[key] = value;
+            }
+        }
+        return current;
+    }
+
+
+    function parseArray() : ASTNode {
+        throw new Error("Function not implemented.");
+    }
+
+    const AST = parseValue();
+    return AST;
 }
